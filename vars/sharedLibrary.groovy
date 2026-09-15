ae083a4ee0b96b019745c7246f773b05849e21ad
tanika <tanika.baliyan1997@gmail.com>
2026-09-15 14:38:51 +0530

    Add Jenkins pipeline definition in sharedLibrary.groovy

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -1,1 +1,21 @@
+def call(map config){
+pipeline{
+agent any //tools,parameters
 
+stages{
+stage ("checkout"){
+steps{
+git branch: 'config.Branch', credentialsId: 'config.cred', url: 'config.repourl' 
+    }    
+}   
+stage('Build'{
+ steps{
+ sh "mvn ${config.mavencommand}"    
+ }   
+} 
+ 
+}//stages closing
+    
+} //pipeline closing   
+    
+}//def closing
