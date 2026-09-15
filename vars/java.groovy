37e3b2a1458a28bbc358bb0060c633351a0baca2
tanika <tanika.baliyan1997@gmail.com>
2026-09-15 15:24:51 +0530

    Refactor Groovy pipeline script for clarity

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,24 +1,25 @@
-def call(map config){
-pipeline{
-agent any //tools,parameters
-tools {
-    maven "${config.maventool}"
+def call(Map config) {
+
+    pipeline {
+        agent any
+
+        tools {
+            maven "${config.maventool}"
+        }
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git branch: config.branch, url: config.repourl
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh "mvn ${config.mavencommand}"
+                }
+            }// stages closing
+        }//pipeline closing
+    } //def closing
 }
-stages{
-stage ("checkout"){
-steps{
-git branch: 'config.branch', credentialsId: 'config.cred', url: 'config.repourl' 
-    }    
-}   
-stage('Build'{
- steps{
- sh "mvn ${config.mavencommand}"    
- }   
-} 
- 
-}//stages closing
-    
-} //pipeline closing   
-    
-}//def closing
-    
