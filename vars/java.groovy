9780bf926873083e5004004cade2cf7c1e7b9a10
tanika <tanika.baliyan1997@gmail.com>
2026-09-15 14:47:25 +0530

    Add Jenkins pipeline script in java.groovy

---
+++ vars/java.groovy
@@ -0,0 +1,22 @@
+def call(map config){
+pipeline{
+agent any //tools,parameters
+
+stages{
+stage ("checkout"){
+steps{
+git branch: 'config.Branch', credentialsId: 'config.cred', url: 'config.repourl' 
+    }    
+}   
+stage('Build'{
+ steps{
+ sh "mvn ${config.mavencommand}"    
+ }   
+} 
+ 
+}//stages closing
+    
+} //pipeline closing   
+    
+}//def closing
+    
